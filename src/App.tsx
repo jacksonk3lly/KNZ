0becd784364b5f7a08e5ea1e387ca1fa5151acfb
Jackson Kelly <jacksonshane04@gmail.com>
2024-11-29 21:09:10 +1300

    more React work

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import Countdown from "./components/Countdown";
+import "./App.css";
 
 function App() {
-  return <Countdown></Countdown>;
+  return (
+    <div className="top-time">
+      <h2>Time Sunny's back in NZ!</h2>
+      <Countdown />
+    </div>
+  );
 }
 
 export default App;
