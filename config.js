52493c3bf72776b99344496db11c7a9934bcef53
Jackson Kelly <jacksonshane04@gmail.com>
2024-12-17 21:04:49 +1300

    added image upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDPVvy12T1WfEkpNeJx-rkyHgFS8_IBR5o",
+  authDomain: "knz-backend.firebaseapp.com",
+  projectId: "knz-backend",
+  storageBucket: "knz-backend.firebasestorage.app",
+  messagingSenderId: "134399990818",
+  appId: "1:134399990818:web:c1edde95d03b479e077e7b",
+  measurementId: "G-0Y5SDT9W8X",
+};
---
+++ main.js
@@ -0,0 +1,33 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
+import {
+  getStorage,
+  ref,
+  uploadBytes,
+  getDownloadURL,
+} from "https://www.gstatic.com/firebasejs/10.11.1/firebase-storage.js";
+
+import { firebaseConfig } from "./config.js";
+
+const app = initializeApp(firebaseConfig);
+const storage = getStorage(app);
+const imagePreview = document.getElementById("imagePreview");
+
+async function retiriveImage() {
+  const storageRef = ref(storage, `images/framephoto`);
+  const imageURL = await getDownloadURL(storageRef);
+  imagePreview.src = imageURL;
+}
+
+async function uploadImage() {
+  const fileInput = document.getElementById("fileInput");
+  const file = fileInput.files[0];
+
+  if (file) {
+    const storageRef = ref(storage, `images/framephoto`);
+    await uploadBytes(storageRef, file);
+  }
+}
+
+const uploadButton = document.getElementById("uploadButton");
+uploadButton.addEventListener("click", uploadImage);
+retiriveImage();
